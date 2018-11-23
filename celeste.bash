1b447b207e520024e87cb0da0e63115b44435bae
Jeremy Banks <_@jeremy.ca>
2018-11-23 13:18:12 -0500

    jbanks@jbanks.local old unstaged

--- celeste.bash
+++ celeste.bash
@@ -2,13 +2,15 @@
 
 cd ~/Library/"Application Support"/Celeste/Saves;
 
-git commit -m "" --allow-empty-message &> /dev/null || true;
+user="$(whoami)@$(hostname)";
 
-git commit . -m "" --allow-empty-message &> /dev/null || true;
+git commit -m "$user old staged" --allow-empty-message &> /dev/null || true;
+
+git commit . -m "$user old unstaged" --allow-empty-message &> /dev/null || true;
 
 git fetch &> /dev/null;
 
-(git pull --ff-only &> /dev/null && echo "Synced") || (
+(git pull --ff-only &> /dev/null && echo "Synced.") || (
     echo "Sync conflict. Archiving remote data and replacing with local." &&
     git pull -s ours --no-edit);
 
@@ -16,7 +18,7 @@ git push &> /dev/null;
 
 open -W ~/Library/"Application Support"/itch/apps/celeste/Celeste.app &> /dev/null || open -W /Applications/Celeste.app;
 
-git commit . -m "" --allow-empty-message &> /dev/null &&
-    echo "Committed new changes." || echo "No changes to commit.";
+git commit . -m "$user new session" --allow-empty-message &> /dev/null &&
+    echo "Saved changes." || echo "No changes to save.";
 
 git push &> /dev/null && echo "Synced.";
