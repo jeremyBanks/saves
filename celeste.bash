895a2069bad7b47e7619cd1320e6963fc05749ee
Jeremy Banks <_@jeremy.ca>
2019-01-05 03:16:58 -0500

    ⚠ jeremy@DESKTOP-1APBBOB old unstaged

--- celeste.bash
+++ celeste.bash
@@ -46,7 +46,6 @@ rm -f 2.celeste.txt; target/debug/celeste-saves 2.celeste 1> 2.celeste.txt 2> /d
 
 if -q git commit . -m "🍓 $user" --allow-empty-message; then
     -qq git push && echo "✅ Synced" || echo "⚠ Sync failed";
-    echo;
     git diff -U2 --ws-error-highlight=none HEAD~1..HEAD *.celeste.txt;
 else
     echo "🆗 No changes to sync"
