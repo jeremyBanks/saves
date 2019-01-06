998e982e91addbcd6983bbbed55685d10bdea58a
Jeremy Banks <_@jeremy.ca>
2019-01-06 01:41:36 -0500

    ⚠ jeremy@DESKTOP-1APBBOB old unstaged

--- celeste.bash
+++ celeste.bash
@@ -46,7 +46,7 @@ rm -f 2.celeste.txt; target/debug/celeste-saves 2.celeste 1> 2.celeste.txt 2> /d
 
 if -qq git commit . -m "🍓 $user" --allow-empty-message || true; then
     -qq git push && echo "✅ Synced" || echo "⚠ Sync failed";
-    yarn run send "$(git diff -U4 --ws-error-highlight=none HEAD~12..HEAD *.celeste.txt | tail -n +6 | grep -v '@@')";
+    yarn run send "$(git diff -U4 --ws-error-highlight=none HEAD~12..HEAD *.celeste.txt | tail -n +6')";
 else
     echo "🆗 No changes to sync"
 fi
