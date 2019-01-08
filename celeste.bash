97da3bf48c86ebcc522a9235729a4e792dd0763a
Jeremy Banks <_@jeremy.ca>
2019-01-07 23:05:00 -0500

    ⚠ jeremy@DESKTOP-1APBBOB old unstaged

--- celeste.bash
+++ celeste.bash
@@ -49,7 +49,7 @@ cat template.html > index.html; for n in 0 1 2; do
         echo -n '<div style="position: absolute; top: 0; right: 0;"><a href="'./${n}.celeste'">'${n}'.celeste</a> </div>'
         CELESTE_SAVE_COLOR=ON target/debug/celeste-saves ${n}.celeste | head -n -1 | node_modules/.bin/ansi-to-html; \
         echo -n "</pre>" \
-    ) | tee --append ${0}.html 1>> index.html 2> /dev/null;
+    ) | tee --append ${n}.html 1>> index.html 2> /dev/null;
 done
 
 -qq git add .;
